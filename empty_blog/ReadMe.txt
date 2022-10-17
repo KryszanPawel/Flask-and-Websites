89ed778cad0f4278791708749068ef7fbe91c7cc
KryszanPawel <114944431+KryszanPawel@users.noreply.github.com>
2022-10-17 20:08:02 +0200

    Add files via upload
    Fix: of contact form in contact.html

--- empty_blog/ReadMe.txt
+++ empty_blog/ReadMe.txt
@@ -1,6 +1,12 @@
 Project is part of 100 Days of Code: The Complete Python Pro Bootcamp for 2022.
 During this project I have learnt a lot about using bootstrap, flask and jinja.
-Website is rendered on flask server, it is styled with bootstrap framework and it works with jinja templates. 
-Header and footer on every site are imported by jinja and posts are automaticli redered by jinja for loop and request module. Script is  downloading posts from npoint API and render them on main website. 
-Full content of post is accesable by clicking on header. Script moves to template site "post.html" where full post is rendered on based on id of clicked title.
-Blog has fully functional navigation bar, sticked to top of website.
+Website is rendered on flask server, it is styled with bootstrap framework and it works with jinja templates.
+Header and footer on every site are imported by jinja and posts are automatically rendered by jinja for loop and request module. Script is downloading posts from npoint API and render them on main website.
+Full content of post is accessible by clicking on header. Script moves to template site "post.html" where full post is rendered on based on id of clicked title.
+Blog has fully functional navigation bar, stuck to top of website.
+
+Update 17/10/22:
+Fix of contact form: contact form sends email through smtplib module with data included in form.
+Header on "/contact" endpoint changes to notify if form was sent successfully or something went wrong during posting form.
+(form uses flask request method GET/POST to work)
+Email is sent using smtplib, my tip is to use hotmail or outlook email as domain sent from. In other cases you woould need to change details in smtlib.SMTP method (gmail.com does not work, it doesn't allow accessing mail by 3rd party application any more.)
--- empty_blog/main.py
+++ empty_blog/main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from datetime import datetime
+import smtplib
+
+USER = ""         #Input dummy email to send contact form from
+PASSWORD = ""              #Password to log in  to dummy email
+YOUR_EMAIL = ""   #Email you want to send notification to.
 
 app = Flask(__name__)
 
-response = requests.get("https://api.npoint.io/7ab7877c456d92deb6d2").json()
+response = requests.get("https://api.npoint.io/7ab7877c456d92deb6d2").json()    # my dummy endpoint with posts
 date = datetime.today().strftime("%Y-%m-%d")
 
-
+def send_email(name,email,phone, message):
+    with smtplib.SMTP("smtp.office365.com", port=587) as connection:    # details for hotmail.com
+        connection.starttls()
+        connection.login(user=USER, password=PASSWORD)
+        connection.sendmail(from_addr=USER,
+                            to_addrs=YOUR_EMAIL,
+                            msg=f"Subject:New Message\n\nName: {name}\nE-mail: {email}\nPhone: {phone}\nMessage: {message}")
 
 @app.route("/")
 def main():
@@ -21,15 +32,29 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        title = 0
+        return render_template("contact.html", title= title)
+    elif request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        if name and email and phone and message:
+            title = "Successfully sent your message"
+            send_email(name, email, phone, message)
+        else:
+            title = "Something went wrong, please fill the form once again"
+        return render_template("contact.html", title=title)
 
 
 @app.route("/post/id/<id>")
 def post(id):
     id = int(id)
     return render_template("post.html", posts=response, id=id, date=date)
+
 
 
 if __name__=="__main__":
